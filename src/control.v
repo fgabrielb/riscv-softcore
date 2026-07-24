54d98420072b98ca09db4cdd48d08fc59ebc88c5
Francisco Gabriel de Oliveira Bezerra <gabhriel.oliveirab@gmail.com>
2026-07-24 18:38:33 -0300

    finished control module

--- src/control.v
+++ src/control.v
@@ -12,11 +12,43 @@ module control (
 
 always @(*) begin
 	case (opcode)
-		7'b 0110011: begin
+		7'b 0110011: begin // R-format
 			alu_src = 0;
 			mem_to_reg = 0;
 			reg_write = 1;
 			mem_read = 0;
 			mem_write = 0;
 			branch = 0;
+			alu_op = 2'b10;
+		end
+		7'b0000011: begin // ld
+			alu_src = 1;
+			mem_to_reg = 1;
+			reg_write = 1;
+			mem_read = 1;
+			mem_write = 0;
+			branch = 0;
+			alu_op = 2'b00;
+		end
+		7'b0100011: begin // sd
+			alu_src = 1;
+			mem_to_reg = 0; // don't care bit
+			reg_write = 0;
+			mem_read = 0;
+			mem_write = 1;
+			branch = 0;
+			alu_op = 2'b00;
+		end
+		7'b1100011: begin // beq
+			alu_src = 0;
+			mem_to_reg = 0;
+			reg_write = 0;
+			mem_read = 0;
+			mem_write = 0;
+			branch = 1;
+			alu_op = 2'b01;
+		end
+	endcase
+end
+endmodule
 			
